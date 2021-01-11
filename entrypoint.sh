2bc641f4e8db695706632dc88b31d96d4d44f352
laura5494 <69025680+laura5494@users.noreply.github.com>
2021-01-11 15:44:14 -0600

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -12,7 +12,7 @@ set -e
 DB_ARGS=()
 function check_config() {
     param="$1"
-    value="$2"    
+    value="$2"
     if grep -q -E "^\s*\b${param}\b\s*=" "$ODOO_RC" ; then       
         value=$(grep -E "^\s*\b${param}\b\s*=" "$ODOO_RC" |cut -d " " -f3|sed 's/["\n\r]//g')
     fi;
